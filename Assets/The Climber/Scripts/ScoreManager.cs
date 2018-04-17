8fe7995c5c1baafca654531615fe9ab32ead720e
guli <guli@tsinghua.org.cn>
2018-04-17 21:00:49 +0800

    base

--- Assets/The Climber/Scripts/ScoreManager.cs
+++ Assets/The Climber/Scripts/ScoreManager.cs
@@ -27,7 +27,7 @@ public class ScoreManager : MonoBehaviour {
 
     [HideInInspector]
     public float bestPointsCountDisp;
-    public Text blanceText;
+    public Text balanceText;
     public Text uidText;
 
     private void Awake()
@@ -71,7 +71,7 @@ public class ScoreManager : MonoBehaviour {
         bestPointsDisp.text = "Best : " + Mathf.Round(bestPointsCountDisp);
 
         uidText.text = "userid : "+App.Instance.uid;
-        blanceText.text = "balance : "+App.Instance.role_balance;
+        balanceText.text = "balance : "+App.Instance.role_balance;
     }
 
     public void AddScore(int pointsToAdd)
